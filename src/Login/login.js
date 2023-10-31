ab8bddf0a910c839fdf441e2f9132516b8a9b151
Sudhiksha Kandavel Rajan <63871993+Sudhiksha-17@users.noreply.github.com>
2023-10-31 13:18:42 +0530

    Initial commit

---
+++ src/Login/login.js
@@ -0,0 +1,44 @@
+// src/components/Login/Login.js
+import React, { useState } from "react";
+import "./login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your authentication logic here
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Gmail Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
